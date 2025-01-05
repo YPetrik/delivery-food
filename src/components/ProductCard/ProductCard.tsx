0b655de35aab2d6f1bcf5dd725769041665b5331
Yuriy <yrii.petrik@gmail.com>
2025-01-05 19:02:11 +0300

    Added ProductCard

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,36 @@
+import { IProductCardProps } from "./ProductCard.props";
+import style from "./ProductCard.module.css";
+import { Link } from "react-router-dom";
+
+export const ProductCard = (props: IProductCardProps) => {
+  const { id, title, description, image, price, rating } = props;
+  return (
+    <Link className={style["link"]} to={"/"}>
+      <div className={style["card"]}>
+        <div
+          className={style["head"]}
+          style={{ backgroundImage: `url(${image})`, borderRadius: "20px", width: "310px" }}
+        >
+          <div className={style["price"]}>
+            {price} &nbsp;
+            <span className={style["currency"]}>$</span>
+          </div>
+          <button className={style["add-to-card"]}>
+            <img
+              className={style["img-bag-shopping"]}
+              src="/public/bag-shopping.svg"
+              alt="bag-shopping"
+            />
+          </button>
+          <div className={style["rating"]}>
+            {rating} &nbsp; <img className={style["star"]} src="/public/star.svg" alt="star" />
+          </div>
+        </div>
+        <div className={style["footer"]}>
+          <div className={style["title"]}>{title}</div>
+          <div className={style["description"]}>{description}</div>
+        </div>
+      </div>
+    </Link>
+  );
+};
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,12 +1,25 @@
 import { Headling } from "../../components/Headling/index";
+import { ProductCard } from "../../components/ProductCard/index";
 import { Search } from "../../components/Search/index";
 import style from "./Menu.module.css";
 
 export const Menu = () => {
   return (
-    <div className={style["head"]}>
-      <Headling>Меню</Headling>
-      <Search className={style['']} placeholder="Введите блюдо или состав"/>
-    </div>
+    <>
+      <div className={style["head"]}>
+        <Headling>Меню</Headling>
+        <Search className={style[""]} placeholder="Введите блюдо или состав" />
+      </div>
+      <div>
+        <ProductCard
+          id={1}
+          title="Наслаждайся"
+          description="Салями, руккола, помидоры и всяка всячина"
+          image='/public/karbonara.png'
+          price={650}
+          rating={4}
+        />
+      </div>
+    </>
   );
 };
